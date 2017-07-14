2440f0d49d5d50e9d33288c7eb083488f039a2e8
Alessandro Giordo <bovas85@gmail.com>
2017-07-14 16:13:07 +0100

    Removed vue-resource and added axios
    vue-resource is deprecated

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    NASA Image Search
     <router-view></router-view>
   </div>
 </template>
@@ -11,13 +10,14 @@ export default {
 }
 </script>
 
+<style lang="sass" src="bulma"></style>
+
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  overflow: hidden
 }
 </style>
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,47 +1,69 @@
 <template>
-  <div class="search">
-    <form v-on:submit.prevent="getResults(query)">
-      <input type="text" placeholder="query" v-model="query" />
-    </form>
-    <div class="results" v-if="results">
-      <div v-for="result in results">
-        <img v-bind:src="result.links[0].href" />
+  <div class="container">
+    <div class="columns">
+      <div class="column is-half is-offset-one-quarter">
+        <h1 class="title">NASA Image Search</h1>
+        <div class="field has-addons">
+          <p class="control">
+            <input class="input" @keydown.enter="getResults(query)" type="text" placeholder="Type your search here..." v-model="query" />
+          </p>
+          <p class="control">
+            <a class="button is-primary" @click="getResults(query)">Search</a>
+          </p>
+          </form>
+        </div>
+        <div class="results" v-if="results.length">
+          <div v-for="result in results" :key="result.data[0].nasa_id">
+            <img :src="result.links[0].href" />
+          </div>
+        </div>
+        <div v-else>
+          <p>Sorry, nothing found for "
+            <strong>{{ query }}</strong>".</p>
+        </div>
       </div>
     </div>
+  
   </div>
 </template>
 
 <script>
-export default {
-  name: 'search',
-  data () {
-    return {
-      query: '',
-      results: ''
-    }
-  },
-  methods: {
-    getResults (q) {
-      q = q.replace(/ /g, '%20')
-      console.log(q)
-      this.$http.get('https://images-api.nasa.gov/search?q=' + q + '&media_type=image').then(response => {
-        this.results = response.body.collection.items
-      }, response => {
-        console.log('Error: ', response)
-      })
+  import axios from 'axios'
+
+  export default {
+    name: 'search',
+    data () {
+      return {
+        query: '',
+        results: ''
+      }
+    },
+    methods: {
+      getResults (q) {
+        q = q.replace(/ /g, '%20')
+        axios.get('https://images-api.nasa.gov/search?q=' + q + '&media_type=image')
+          .then(response => {
+            this.results = response.data.collection.items
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.search {
-  margin: 20px auto;
-}
 
-.results img {
-  height: 300px;
-  margin: 10px;
-}
+  .field.has-addons {
+    justify-content: center;
+  }
+  
+  .results {
+    max-height: 70vh;
+    overflow-y: auto;
+    margin-top: 20px;
+  }
+ 
 </style>
